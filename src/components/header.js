6fc50ebc69a8547162d06e56457afd249d32df78
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-05-04 01:14:05 +0530

    icon

--- src/components/header.js
+++ src/components/header.js
@@ -6,16 +6,16 @@ return (<Navbar bg="light" expand="lg">
         {/* <Navbar.Brand href="#home"><b>Amit vanity van</b></Navbar.Brand> */}
         <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          <img src="https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/logo/logo.png?raw=true" width="60" height="30" className="d-inline-block align-top" alt=""></img>
-          <b style={{paddingLeft: "10px"}}>Amit Vanity Van</b>
+          <img src="https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/logo/logo.png?raw=true" width="100" height="30" className="d-inline-block align-top" alt=""></img>
+          <b style={{paddingLeft: "10px"}}>AMIT VANITY VAN</b>
         </a>
         </nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="/contact#contact">Contact Us</Nav.Link>
-            <Nav.Link href="/home#services">Services</Nav.Link>
+            <Nav.Link href="home"><b>Home</b></Nav.Link>
+            <Nav.Link href="/contact#contact"><b>Contact Us</b></Nav.Link>
+            <Nav.Link href="/home#services"><b>Services</b></Nav.Link>
           </Nav>
         </Navbar.Collapse>
 </Navbar>);
