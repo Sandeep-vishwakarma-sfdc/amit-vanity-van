83de4c193cf13283754504eb23cbb53acaaa4916
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-05-03 23:57:31 +0530

    final

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,13 @@ import {  Navbar, Nav} from 'react-bootstrap';
 function Header(){
 
 return (<Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home"><b>Amit vanity van</b></Navbar.Brand>
+        {/* <Navbar.Brand href="#home"><b>Amit vanity van</b></Navbar.Brand> */}
+        <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img src="https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/logo/logo.png?raw=true" width="60" height="30" className="d-inline-block align-top" alt=""></img>
+          <b style={{paddingLeft: "10px"}}>Amit Vanity Van</b>
+        </a>
+        </nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
