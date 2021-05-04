c2613bd6caf3047040c5c0be52776fa5634eae31
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-05-04 12:20:47 +0530

    header fixed

--- src/components/header.js
+++ src/components/header.js
@@ -5,34 +5,34 @@ function Header(){
 
 return (
 <div className="stickyheader">
-  <div className="container-fluid ">
+  <div className="container-fluid" style={{backgroundColor:"#17202A"}}>
     <div className="row">
       <div className="col-sm-12 col-md-4">
-      <MdEmail/>
-      <a style={{margin:"5px"}} href="mailto:amitvanityvan@gmail.com">amitvanityvan@gmail.com 
+      <MdEmail color="#fff"/>
+      <a className="headerlink" href="mailto:amitvanityvan@gmail.com">amitvanityvan@gmail.com 
       </a> 
       </div>
       <div className="col-sm-12 col-md-4">
-      <FaPhoneAlt/>
-      <a style={{margin:"5px"}} href="tel:+919769061832">9769061832 
-      </a> / <a style={{margin:"5px"}} href="tel:+919892796375">9892796375 
+      <FaPhoneAlt color="#fff"/>
+      <a className="headerlink" href="tel:+919769061832">9769061832 
+      </a> / <a className="headerlink" href="tel:+919892796375">9892796375 
       </a>
       </div>
       <div className="col-sm-12 col-md-4">
       <a title="facebook" style={{margin:"5px"}} href="https://www.facebook.com/Amitvanityvan/" target="_blank"> 
-      <FaFacebookF/>
+      <FaFacebookF color="#fff"/>
       </a>
       <a title="twitter" style={{margin:"5px"}} href="https://twitter.com/amitvish786" target="_blank"> 
-      <FaTwitter/>
+      <FaTwitter color="#fff"/>
       </a>
       <a title="instagram" style={{margin:"5px"}} href="https://www.instagram.com/amitvanityvan/" target="_blank"> 
-      <FaInstagram/>
+      <FaInstagram color="#fff"/>
       </a>
       <a title="linkedin" style={{margin:"5px"}} href="https://in.linkedin.com/in/amit-vanity-van-4151b0138" target="_blank"> 
-      <FaLinkedinIn/>
+      <FaLinkedinIn color="#fff"/>
       </a>
       <a title="youtube" style={{margin:"5px"}} href="https://www.youtube.com/channel/UCbDf6KxhFq9LzDYPcljO6oQ" target="_blank"> 
-      <FaYoutube/>
+      <FaYoutube color="#fff"/>
       </a>
       </div>
     </div>
@@ -43,7 +43,7 @@ return (
         <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           <img src="https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/logo/logo.png?raw=true" width="60" height="30" className="d-inline-block align-top" alt=""></img>
-          <b style={{paddingLeft: "10px"}}>AMIT VANITY VAN</b>
+          <b style={{paddingLeft: "10px",fontSize:"14px",fontFamily:"'Roboto', sans-serif"}}>AMIT VANITY VAN</b>
         </a>
         </nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
