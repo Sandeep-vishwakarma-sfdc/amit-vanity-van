1ef850b3dab97a835733ac9a7c5fbd4115d109a1
Sandeep-vishwakarma-sfdc <sandeep.vishwakarma.sfdc@gmail.com>
2023-11-14 01:30:45 +0530

    fixed css

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,15 +1,18 @@
 
 import Button from "@material-ui/core/Button";
-const Card = ({ id, img, description }) => {
+const Card = ({ id, img, description,parentId}) => {
     return (
       <div className="card">
         <a href={`\\vanityvan\\${id}`}>
         <div className="card-upper">
           <img src={img} alt={`Image ${id}`} />
         </div>
+        
         <div className="card-lower">
           <p>{description}</p>
-          <Button href="https://api.whatsapp.com/send?phone=919769061832&text=Hi%21%20i%20have%20visited%20your%20website" variant="outlined" color="primary">Get Quote Now</Button>
+          <Button href="https://api.whatsapp.com/send?phone=919769061832&text=Hi%21%20i%20have%20visited%20your%20website" variant="outlined" color="primary" target="_blank" style={{marginLeft: '0.8rem',marginTop:'0.8rem' }}>Get Quote Now</Button>
+
+          {parentId?(<div></div>):(<Button href={`\\vanityvan\\${id}`} variant="outlined" style={{marginLeft: '0.8rem',marginTop:'0.8rem' }} color="primary">View Detail</Button>)}
         </div>
         </a>
       </div>
