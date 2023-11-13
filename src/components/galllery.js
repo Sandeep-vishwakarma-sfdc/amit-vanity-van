c58738563ce12bb478c3b628fa493338b5e210c7
Sandeep-vishwakarma-sfdc <sandeep.vishwakarma.sfdc@gmail.com>
2023-11-13 15:39:03 +0530

    Test

--- src/components/galllery.js
+++ src/components/galllery.js
@@ -6,6 +6,7 @@ function Gallary(){
     const [vanities,setVanities] = useState([{id:101,img:'https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/blue/e2.jpeg?raw=true'},{id:102,img:'https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/darkblue/e2.jpeg?raw=true'},{id:103,img:'https://github.com/Sandeep-vishwakarma-sfdc/image-repo/blob/master/white/e1.jpeg?raw=true'}])  
 
     return (<Container>
+      <div>Test</div>
       <div className='row'>
         {vanities.map((van)=>{
         return (<div className="" key={van.id}>
