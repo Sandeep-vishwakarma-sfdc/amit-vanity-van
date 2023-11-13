a23b6de071508ac3629dbdc86bd9f362df7254fd
Sandeep-vishwakarma-sfdc <sandeep.vishwakarma.sfdc@gmail.com>
2023-11-14 01:39:38 +0530

    added code to whatsApp message

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,6 +1,8 @@
 
 import Button from "@material-ui/core/Button";
 const Card = ({ id, img, description,parentId}) => {
+  let pCode = parentId?parentId:id;
+  let whatsappLink = 'https://api.whatsapp.com/send?phone=919769061832&text=Hi%21%20i%20have%20visited%20your%20website%20I%20am%20interested%20in%20Product%20Code%20'+pCode;
     return (
       <div className="card">
         <a href={`\\vanityvan\\${id}`}>
@@ -10,7 +12,7 @@ const Card = ({ id, img, description,parentId}) => {
         
         <div className="card-lower">
           <p>{description}</p>
-          <Button href="https://api.whatsapp.com/send?phone=919769061832&text=Hi%21%20i%20have%20visited%20your%20website" variant="outlined" color="primary" target="_blank" style={{marginLeft: '0.8rem',marginTop:'0.8rem' }}>Get Quote Now</Button>
+          <Button href={whatsappLink} variant="outlined" color="primary" target="_blank" style={{marginLeft: '0.8rem',marginTop:'0.8rem' }}>Get Quote Now</Button>
 
           {parentId?(<div></div>):(<Button href={`\\vanityvan\\${id}`} variant="outlined" style={{marginLeft: '0.8rem',marginTop:'0.8rem' }} color="primary">View Detail</Button>)}
         </div>
